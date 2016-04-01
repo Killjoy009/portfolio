523a5b68eeb76b7e3d202f9b4d20809b00022699
frejemaes <crudajohnrobert@yahoo.com>
2016-04-02 03:28:11 +0800

    index php

---
+++ index.php
@@ -0,0 +1,208 @@
+<?php include 'header.php'; ?>
+
+	<div class="level level-hero hero-home has-hint">
+		
+
+		<div class="hero-overlay"></div>
+
+		<video loop id=bg-video class=fullscreen-video>
+			<source src="img/video.mp4" type="video/mp4">
+		</video>
+
+		<div class="container full-height">
+			<div class=v-align-parent>
+				<div class=v-center>
+					<div class="row">			
+						<section class="cd-intro">
+							<h1 class="cd-headline letters type">
+								<span>Hi I'm</span> 
+								<span class="cd-words-wrapper waiting">
+									<b class="is-visible">Web Developer</b>
+									<b>ROBERT CRUDA</b>
+								</span>
+							</h1>
+						</section> <!-- cd-intro -->
+					</div>
+				</div>
+			</div>
+		</div>
+
+			<div class=hint-arrow></div> <!-- arrow-icon hint -->
+	
+	</div>
+	
+	<div class=level>
+
+		<div class="container mb-100 xs-mb-40">
+			<div class=row>
+				<div class="col-sm-5 col-md-4 col-md-offset-2 col-sm-offset-1 wow fadeInLeft">
+					<h1 class="mb-10 xs-mb-10 heading color-dark heading-bordered">My Super<br class=hidden-xs> Power</h1>
+				</div>
+				<div class="col-sm-5 col-sm-offset-1 wow fadeInRight">
+					<h2 class="w-300 mb-10 color-black xs-mb-10">Is to Make Web a Better Place</h2>
+					<p class="xs-mw">I'am a wizard of tommorow i use code as my spell to bring my art to life.
+				</div>
+			</div>
+		</div>
+
+		<img src="img/v2/webmockup.png" alt class="mb-80 hidden-xs iphone-inline wow fadeInUp delay-05s">
+
+		<div class=container>
+			<div class="row mb-90 xs-mb-0">
+
+				<div class="col-sm-1 col-sm-offset-2 wow fadeInLeft">
+					<i class="icon fa fa-html5 fa-lg"></i>
+				</div>
+				<div class="col-sm-3 wow fadeInLeft">
+					<h3 class="mb-15">HTML5</h3>
+					<p class="smaller xs-mb-40 xs-mw">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+				</div>
+
+				<div class="col-sm-1 wow fadeInRight">
+					<i class="icon fa fa-css3 fa-lg"></i>
+				</div>
+				<div class="col-sm-3 wow fadeInRight">
+					<h3 class="mb-15">CSS3</h3>
+					<p class="smaller xs-mb-40 xs-mw">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+				</div>
+
+			</div>
+
+			<div class=row>
+
+				<div class="col-sm-1 col-sm-offset-2 wow fadeInLeft delay-05s">
+					<i class="icon fa fa-jsfiddle fa-lg"></i>
+				</div>
+				<div class="col-sm-3 wow fadeInLeft delay-05s">
+					<h3 class="mb-15">JQuery</h3>
+					<p class="smaller xs-mb-40 xs-mw">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+				</div>
+
+				<div class="col-sm-1 wow fadeInRight delay-05s">
+					<i class="icon fa fa-wordpress fa-lg"></i>
+				</div>
+				<div class="col-sm-3 wow fadeInRight delay-05s">
+					<h3 class="mb-15">Wordpress</h3>
+					<p class="smaller xs-mb-40 xs-mw">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="level level-img-right">
+
+		<div class="container xs-mb-30">
+			<img class="img-right-sprite wow fadeInUp delay-05s"  src="img/v2/iphone-bg-lg.min.png" alt>
+			<div class="row mb-60 xs-mb-20 wow fadeInUp">
+				<div class="col-sm-6 col-lg-offset-1 col-lg-5">
+					<h1 class="mb-20 xs-mb-10 heading color-dark heading-bordered xl-heading-outdent">Details</h1>
+					<h2 class="w-300 color-black mb-10">All what you want in your website</h2>
+					<p class=xs-mw>Now with Appi you have it on your fingertips.
+				</div>
+			</div>
+
+			<div class="row mb-60 xs-mb-20 wow fadeInLeft">
+				<div class="col-sm-1 col-lg-offset-1">
+					<i class="icon fa fa-globe fa-lg"></i>
+				</div>
+				<div class="col-sm-4 col-md-3">
+					<h3 class="mb-15">Cross Browser Compatibility</h3>
+					<p class="smaller xs-mw">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+				</div>
+			</div>
+
+			<div class="row mb-60 xs-mb-20 wow fadeInLeft">
+				<div class="col-sm-1 col-lg-offset-1">
+					<i class="icon fa fa-clone fa-lg"></i>
+				</div>
+				<div class="col-sm-4 col-md-3">
+					<h3 class="mb-15">Responsive Layout</h3>
+					<p class="smaller xs-mw">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+				</div>
+			</div>
+			<div class="row wow fadeInLeft">
+				<div class="col-sm-1 col-lg-offset-1">
+					<i class="icon fa fa-mobile fa-lg"></i>
+				</div>
+				<div class="col-sm-4 col-md-3">
+					<h3 class="mb-15">Mobile First Design</h3>
+					<p class="smaller xs-mw">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="level" id="about">
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-md-6 text-center">
+				<img src="img/developer.jpg" width="90%">
+
+			</div>
+
+			<div class="col-md-6">
+				<h2 class="wow fadeInRight">Logic will Get you from A to B. 
+				<b class="color-dark">Imagination Will Take You Everywhere</b></h2>
+				<br/>
+				<br/>
+				<blockquote class="wow fadeInRight delay-05s">
+					I like to push the boundaries. I love creating creative, clean, usable design for the web
+					and other digital sources.
+					I would portray "Design"
+					in a simple sense as my obsession. It is something that I enjoy being involved with in many different aspects. It keeps me driven,
+					it keeps me going, it is something i see myself being involved in for a long time.It's something I do before going to bed, and something I can't wait to do in the morning.
+				</blockquote>
+
+				<p class="wow fadeInRight delay-05s">John Robert Cruda - Web Designer / Developer</p>
+
+	            <div class="col-sm-1 col-lg-offset-1 wow fadeInRight delay-05s">
+                    <a href="cruda-resume.docx" class="btn btn-lg btn-outline">
+                      <i class="fa fa-download" download="Resume"></i> View My Resume
+                    </a>
+                </div>
+			</div>
+
+			</div>
+
+
+			</div>
+
+	</div>
+
+	<div id="works" class="level level-outro text-center">
+		<div class=container>
+			<h1 class="mb-10 color-dark  heading">Projects</h1>
+		</div>
+
+		<div class="cd-testimonials-wrapper cd-container" id="works">
+			<ul class="cd-testimonials">
+				<li>
+					<a href="http://pixelflex.x10.mx">
+					<img src="img/project-1.png" class="project-img">
+					</a>
+				</li>
+		
+				<li>
+					<a href="#0">
+					<img src="img/project-3.png" class="project-img">
+					</a>
+				</li>
+		
+				<li>
+					<a href="#0">
+					<img src="img/project-2.png" class="project-img">
+					</a>
+				</li>
+		
+			</ul> <!-- cd-testimonials -->
+
+		</div> <!-- cd-testimonials-wrapper -->
+	</div>
+
+<?php include 'footer.php'; ?>	
+
